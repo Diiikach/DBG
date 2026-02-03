b1114c59ae399688c2289a4d83896d07c3574c4d
Dmitii Kachaev <dikach@MacBook-Air-Dmitii.local>
2026-02-03 23:35:09 +0300

    added: two migrations

---
+++ db_init/001_create_patient.sql
@@ -0,0 +1,16 @@
+CREATE TYPE sex_enum AS ENUM ('male', 'female', 'other');
+
+CREATE TABLE IF NOT EXISTS patient (
+    patient_id SERIAL PRIMARY KEY,
+    external_id VARCHAR(255) UNIQUE NOT NULL,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    date_of_birth DATE NOT NULL,
+    sex sex_enum NOT NULL,
+    phone_number VARCHAR(20),
+    email VARCHAR(100),
+    address TEXT,
+    phenotype_description TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
